4c7ece32de931a83112f0cad2589e8850e877598
Dipakshi26 <dipakshisaxena26@gmail.com>
2022-08-02 13:43:24 +0530

    Add project files.

---
+++ ConsumeEmployeeWebApi.WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeEmployeeWebApi.WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Uri baseUri = new Uri("https://localhost:7112/api");
+
+        HttpClient client = new HttpClient();
+
+        List<EmployeeViewModel> employeeList = new List<EmployeeViewModel>();
+
+
+        public IActionResult Index()
+        {
+            client.BaseAddress = baseUri;
+            HttpResponseMessage response = client.GetAsync(baseUri + "/employee").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string EmployeeData = response.Content.ReadAsStringAsync().Result;
+                employeeList = JsonConvert.DeserializeObject<List<EmployeeViewModel>>(EmployeeData); ;
+
+            }
+
+            return View(employeeList);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult CreateNewEmployee(EmployeeViewModel employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7112/api/employee");
+                var post = client.PostAsJsonAsync<EmployeeViewModel>("employee", employee);
+                post.Wait();
+                var result = post.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+
+                }
+                ModelState.AddModelError(string.Empty, "Server Error");
+                return View(employee);
+            }
+        }
+
+        public ActionResult Update(int id)
+        {
+            client.BaseAddress = baseUri;
+            HttpResponseMessage response = client.GetAsync(baseUri + "/Employee").Result;
+            string data = response.Content.ReadAsStringAsync().Result;
+            employeeList = JsonConvert.DeserializeObject<List<EmployeeViewModel>>(data);
+            var emp = employeeList.Where(e => e.id == id).FirstOrDefault();
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Save(EmployeeViewModel employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7112/api/employee");
+                var put = client.PutAsJsonAsync($"Employee?empId={employee.id}", employee);
+                put.Wait();
+                var result = put.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            ModelState.AddModelError(string.Empty, "server error");
+            return View();
+
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7112/api/employee");
+                var put = client.DeleteAsync($"Employee?empId={id}");
+                put.Wait();
+                var result = put.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            ModelState.AddModelError(string.Empty, "server error");
+            return View();
+
+        }
+
+    }
+    
+}
---
+++ ConsumeEmployeeWebApi.WebApp/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsumeEmployeeWebApi.WebApp
+{
+    public class EmployeeViewModel
+    {
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
